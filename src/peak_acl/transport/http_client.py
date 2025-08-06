ddbc2cb2c1734411e12c98e3c6f78db59bff073d
Santiago-1211173 <129299267+Santiago-1211173@users.noreply.github.com>
2025-08-06 11:26:00 +0100

    chore: replace MIT headers with GPLv3 notice

--- src/peak_acl/transport/http_client.py
+++ src/peak_acl/transport/http_client.py
@@ -1,6 +1,20 @@
-# MIT License
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2025 Santiago Bossa
-# See LICENSE file in the project root for full license text.
+#
+# This file is part of peak-acl.
+#
+# peak-acl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# peak-acl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with peak-acl.  If not, see the LICENSE file in the project root.
 
 """
 Asynchronous FIPA HTTP-MTP client.
